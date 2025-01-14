2495868bc97b05f64d89797007c9be3a802c40f1
LH10 <lalit1652h@gmail.com>
2025-01-14 14:02:40 +0530

    Changed it so that it can work on any system with mysql taking user input for mysql uname and pass

--- PKG/config/app.go
+++ PKG/config/app.go
@@ -1,7 +1,7 @@
 package config
 
 import(
-	
+	"fmt"
 	"errors"
 	"github.com/jinzhu/gorm"
 	_"github.com/jinzhu/gorm/dialects/mysql"
@@ -10,7 +10,8 @@ import(
 var  ( db *gorm.DB
 )
 func DbConnect() (error){
-	d,err:=gorm.Open("mysql","root:sqlsys@tcp(127.0.0.1:3306)/bookstore?charset=utf8&parseTime=True&loc=Local")
+	mysqlString:=userString()
+	d,err:=gorm.Open("mysql",mysqlString)
 	if err!=nil{
 		return errors.New("Error occured during connection")
 	}
@@ -21,3 +22,16 @@ func GetDb() *gorm.DB{
 	return db
 }
 
+func userString() string {
+	var username,pass,credString,databaseName string
+	fmt.Printf("Enter mysql username:")
+	fmt.Scan(&username)
+	fmt.Printf("Enter corresponding password:")
+	fmt.Scan(&pass)
+	fmt.Printf("Enter database name:")
+	fmt.Scan(&databaseName)
+	credString=fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?charset=utf8&parseTime=True&loc=Local",username,pass,databaseName)
+	fmt.Println("\n-----------------Done-----------------")
+	return credString
+
+}
