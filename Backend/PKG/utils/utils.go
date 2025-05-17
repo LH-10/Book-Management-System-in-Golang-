4fdd92a2e996dc811b9930d279c64d919c28b421
LH-10 <lalit1652h@gmail.com>
2025-05-17 17:43:45 +0530

    Added new util for easy json response

--- Backend/PKG/utils/utils.go
+++ Backend/PKG/utils/utils.go
@@ -4,6 +4,7 @@ import(
 	"io"
 	"encoding/json"
 	"net/http"
+	"fmt"
 )
 
 func ParseBody(r *http.Request,x interface{}){
@@ -11,5 +12,26 @@ func ParseBody(r *http.Request,x interface{}){
 		if err:=json.Unmarshal([]byte(body),x) ; err!=nil{
 			return
 		}
+	}
+}
+
+func MakeResponseJson(w *http.ResponseWriter,data map[string]interface{},isSuccess bool){
+	(*w).Header().Set("Content-Type", "application/json")
+	jsonError:=fmt.Sprintf("%v",(data["error"]))
+	jsonResponse,err:=json.Marshal(data)
+	parseError:=" server error while responding" 
+	
+	if err!=nil{
+	http.Error((*w), parseError, http.StatusInternalServerError)
+	return
+	}
+	if !isSuccess{
+		http.Error((*w),jsonError,http.StatusBadRequest)
+		return
+	}
+	_,err=(*w).Write(jsonResponse)
+	if err!=nil{
+	http.Error((*w), parseError, http.StatusInternalServerError)
+	return
 	}
 }
